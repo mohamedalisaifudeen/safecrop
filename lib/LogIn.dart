2e4c262d36059886f111c239435ebe5da07e687d
mohamedalisaifudeen <mohamedalisaifudeen1@yahoo.com>
2025-02-08 20:20:00 +0530

    Implimented the map Gui and code refactoring

--- lib/LogIn.dart
+++ lib/LogIn.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'InputTxt.dart';
 import "CustomBtn.dart";
-
-
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -12,180 +11,181 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  bool? farmercheck=false;
-  bool? officercheck=false;
+  bool? farmercheck = false;
+  bool? officercheck = false;
+  String email="";
+  String password="";
+  bool emailError=false;
+  bool passError=false;
 
-  final GlobalKey<FormState> _formKey=GlobalKey<FormState>();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Future<void> signInUser(String email, String password) async {
+    try {
+      final credencial = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      print("Login Sucess");
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "user-not-found") {
+        print("No user found for that email");
+        setState(() {
+          emailError=true;
+        });
+      } else if (e.code == "wrong-password") {
+        print("Wrong password provided for that user.");
+        setState(() {
+          passError = true;
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-          child:Column(
-            children: [
-
-              Center(
-                child:Image(image: AssetImage(
-                    "assets/Logo.png"
-                )
-                ),
-              ),
-              Expanded(
-                  child: Form(
-                    key:_formKey,
-                      child: SingleChildScrollView(
+        child: Column(children: [
+          Center(
+            child: Image(image: AssetImage("assets/Logo.png")),
+          ),
+          Expanded(
+              child: Form(
+                  key: _formKey,
+                  child: SingleChildScrollView(
                       child: Column(
-                      children: [
-    Container(
-    margin: EdgeInsets.only(left: 50,right: 50,bottom: 60,top: 40),
-    child:Column(
-    children: [
+                    children: [
+                      Container(
+                          margin: EdgeInsets.only(
+                              left: 50, right: 50, bottom: 60, top: 40),
+                          child: Column(
+                            children: [
+                              TxtInput(
+                                labelTxt: "Email",
+                                password: false,
+                                validator: (String? value) {
+                                  if (value == null || value.isEmpty||
+                                      !value.contains("@") ||
+                                      !value.contains(".com")) {
+                                    return "Please enter a valid email id ";
+                                  }else if(emailError){
+                                    return "No user found for that email";
+                                  }
+                                  return null;
+                                },
+                                value: (value) {
+                                  email=value;
+                                },
+                              ),
+                              TxtInput(
+                                labelTxt: "Password",
+                                password: true,
+                                validator: (String? value) {
+                                  if (value == null || value.isEmpty) {
+                                    return "Please enter some text";
+                                  }else if(passError){
+                                    return "Wrong password provided for that user.";
+                                  }
+                                  return null;
+                                },
+                                value: (value) {
+                                  password=value;
+                                },
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    margin: EdgeInsets.only(
+                                        left: 5, bottom: 10, top: 20),
+                                    child: Text(
+                                      "I am a,",
+                                      style: TextStyle(
+                                          fontSize: 25,
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Checkbox(
+                                            activeColor: Colors.green,
+                                            side: BorderSide(
+                                                color: Colors.green, width: 3),
+                                            value: farmercheck,
+                                            onChanged: (Value) {
+                                              setState(() {
+                                                officercheck = false;
+                                                farmercheck = Value;
+                                              });
+                                            },
+                                          ),
+                                          Text(
+                                            "Farmer",
+                                            style: TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.w400),
+                                          )
+                                        ],
+                                      ),
+                                      Row(
+                                        children: [
+                                          Checkbox(
+                                            activeColor: Colors.green,
+                                            side: BorderSide(
+                                                color: Colors.green, width: 3),
+                                            value: officercheck,
+                                            onChanged: (vaue) {
+                                              setState(() {
+                                                farmercheck = false;
+                                                officercheck = vaue;
+                                              });
+                                            },
+                                          ),
+                                          Text(
+                                            "Officer",
+                                            style: TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.w400),
+                                          )
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                  CustomBtn(
+                                    click: () {
+                                      final bool isvalid =
+                                          _formKey.currentState?.validate() ??
+                                              false;
+                                      if (!isvalid) {
+                                        return;
+                                      };
+                                      signInUser(email, password);
+                                    },
 
-      TxtInput(labelTxt: "Email",
-        password: false,
-        validator:(String?value){
-        if(value==null || value.isEmpty){
-          return "Please enter some text";
-        }
-        return null;
-      },
-        value: (value){
-          print(value);
-        },
+                                    txt: "Log In",
+                                    color: Colors.green,
+
+                                  ),
+                                  CustomBtn(
+                                    click: () {
+                                      Navigator.pushNamed(context, '/signUp');
+                                    },
+                                    txt: "Go to Sign Up",
+                                    color: Colors.teal,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          )),
+                    ],
+                  ))))
+        ]),
       ),
-    TxtInput(labelTxt: "Password",
-      password: true,
-      validator: (String?value){
-      if(value==null || value.isEmpty){
-        return "Please enter some text";
-      }
-      return null;
-    },
-      value: (value){
-        print(value);
-      },
-    ),
-
-    Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-
-    children: [
-    Container(
-    margin: EdgeInsets.only(left: 5,bottom: 10,top: 20),
-    child: Text("I am a,",
-    style: TextStyle(
-    fontSize: 25,
-    fontWeight: FontWeight.w700
-    ),
-    ),
-    ),
-    Row(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: [
-    Row(
-    children: [
-    Checkbox(
-    activeColor: Colors.green,
-    side: BorderSide(
-    color: Colors.green,
-    width: 3
-    ),
-    value: farmercheck,
-    onChanged: (Value){
-    setState(() {
-    officercheck=false;
-    farmercheck=Value;
-    });
-
-
-
-    },
-
-    ),
-    Text(
-    "Farmer",
-    style: TextStyle(
-    fontSize: 20,
-    fontWeight: FontWeight.w400
-    ),
-    )
-    ],
-    ),
-
-    Row(
-    children: [
-    Checkbox(
-    activeColor: Colors.green,
-    side: BorderSide(
-    color: Colors.green,
-    width: 3
-    ),
-    value:officercheck,
-    onChanged: (vaue){
-    setState(() {
-    farmercheck=false;
-    officercheck=vaue;
-    }
     );
-
-    },
-
-    ),
-    Text(
-    "Officer",
-    style: TextStyle(
-    fontSize: 20,
-    fontWeight: FontWeight.w400
-    ),
-    )
-    ],
-    )
-
-    ],
-    ),
-    CustomBtn(click: (){
-    final bool isvalid=_formKey.currentState?.validate()??false;
-    if(!isvalid){
-    return;
-    }
-    },
-    txt: "Log In",
-    color: Colors.green,
-    ),
-
-    CustomBtn(click: (){
-    Navigator.pushNamed(context, '/signUp');
-    },
-    txt: "Go to Sign Up",
-    color: Colors.teal,
-    ),
-
-
-    ],
-    ),
-
-    ],
-    )
-    ),
-    ],
-    )
-    )
-    )
-              )]
-
-    ),
-    ),
-    );
-
-
-
   }
 }
-
-
-
-
-
-
---
+++ lib/OsmFooter.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import "OsmMap.dart";
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class OsmFooter extends StatelessWidget {
+  double lat;
+  double long;
+  OsmFooter({required this.lat,required this.long});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        OsmMap(
+            lat: lat,
+            long: long
+        ),
+        Container(
+          decoration: BoxDecoration(
+              color: Colors.green,
+              borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(30)
+              )
+          ),
+
+          height: MediaQuery.of(context).size.height/4,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Alert ID: 0001XXXXXXXX",
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight:FontWeight.w500
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                TextButton(onPressed: ()async{
+                  final String googleMapsUrl = "https://www.google.com/maps/search/?api=1&query=$lat,$long";
+                  if(await canLaunch(googleMapsUrl)){
+                    await launch(googleMapsUrl);
+                  }else{
+                    print("Couldnt open");
+                  }
+                }, child: Padding(
+                  padding: EdgeInsets.all(11),
+                  child: Text("Navigate",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 27,
+                    ),
+                  ),
+                ),
+                  style: ButtonStyle(
+                      side: MaterialStateProperty.all<BorderSide>(
+                          BorderSide(
+                              color: Colors.white,
+                              width: 3
+                          )
+                      )
+                  ),)
+              ]
+
+
+          ),
+        )
+      ],
+    );
+  }
+}
