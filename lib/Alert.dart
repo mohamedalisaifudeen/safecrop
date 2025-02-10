a4d302ee834a6ccad69ef457afb0d710fb64d787
Halgamaoshen <oshenhalgama@gmail.com>
2025-02-10 13:51:26 +0530

    Alert code

---
+++ lib/Alert.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CustomBtn extends StatelessWidget {
+  VoidCallback click;
+  String txt;
+  Color color;
+  CustomBtn({required this.click, required this.txt, required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Container(
+        margin: EdgeInsets.only(top: 20),
+        decoration: BoxDecoration(
+            color: color, borderRadius: BorderRadius.circular(10)),
+        width: MediaQuery.of(context).size.width - 80,
+        child: TextButton(
+          onPressed: click,
+          child: Text(
+            txt,
+            style: TextStyle(
+                color: Colors.white, fontSize: 22, fontWeight: FontWeight.w700),
+          ),
+        ),
+      ),
+    );
+  }
+}
