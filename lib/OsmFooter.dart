f5a98aa9e13187ac74a287d9c4f98ffe58b26c9b
unknown <shenonkavinda214@gmail.com>
2025-03-03 08:58:11 +0530

    Osm footer final

--- lib/OsmFooter.dart
+++ lib/OsmFooter.dart
@@ -6,43 +6,57 @@ import 'package:url_launcher/url_launcher.dart';
 class OsmFooter extends StatelessWidget {
   double lat;
   double long;
-  OsmFooter({required this.lat,required this.long});
+  OsmFooter({required this.lat, required this.long});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        OsmMap(
-            lat: lat,
-            long: long
-        ),
+        OsmMap(lat: lat, long: long),
         Container(
           decoration: BoxDecoration(
               color: Colors.green,
-              borderRadius: BorderRadius.vertical(
-                  top: Radius.circular(30)
-              )
-          ),
-
-          height: MediaQuery.of(context).size.height/4,
+              borderRadius: BorderRadius.vertical(top: Radius.circular(30))),
+          height: MediaQuery.of(context).size.height / 4,
           width: MediaQuery.of(context).size.width,
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Alert ID: 0001XXXXXXXX",
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Text(
+              "Alert ID: 0001XXXXXXXX",
+              style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            TextButton(
+              onPressed: () async {
+                final String googleMapsUrl =
+                    "https://www.google.com/maps/search/?api=1&query=$lat,$long";
+                if (await canLaunch(googleMapsUrl)) {
+                  await launch(googleMapsUrl);
+                } else {
+                  print("Couldnt open");
+                }
+              },
+              child: Padding(
+                padding: EdgeInsets.all(11),
+                child: Text(
+                  "Navigate",
                   style: TextStyle(
-                      fontSize: 25,
-                      color: Colors.white,
-                      fontWeight:FontWeight.w500
+                    color: Colors.white,
+                    fontSize: 27,
                   ),
                 ),
-                SizedBox(
-                  height: 30,
-                ),
-           ]
-          )
+              ),
+              style: ButtonStyle(
+                  side: MaterialStateProperty.all<BorderSide>(
+                      BorderSide(color: Colors.white, width: 3))),
+            )
+          ]),
         )
-      ]
-    )                     
-  }  
+      ],
+    );
+  }
 }
