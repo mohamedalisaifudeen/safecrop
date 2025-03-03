bb0b32905769878a0568407b3738f633a416c921
inuka2003 <inukawickramasinghe2003@gmail.com>
2025-03-03 20:21:33 +0530

    make bottom nav bar as a stateful widget that can navigate inro pages

--- lib/bottom_nav_bar.dart
+++ lib/bottom_nav_bar.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
 
-class BottomNavBar extends StatelessWidget {
+class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
+
+  @override
+  _BottomNavBarState createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    // Handle navigation here if necessary
+  }
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      currentIndex: 0,
+      currentIndex: _selectedIndex,
       type: BottomNavigationBarType.fixed,
+      onTap: _onItemTapped, // Add this
       items: const [
         BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
         BottomNavigationBarItem(
