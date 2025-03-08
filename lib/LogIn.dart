5669bea7507422174f85927bba8d55b7ccfd64de
inuka2003 <inukawickramasinghe2003@gmail.com>
2025-03-08 10:32:03 +0530

    make the firestore and FCM feature to get alerts to device

--- lib/LogIn.dart
+++ lib/LogIn.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'InputTxt.dart';
 import "CustomBtn.dart";
 
-
-
 class Login extends StatefulWidget {
   const Login({super.key});
 
@@ -12,143 +10,111 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  bool? farmercheck=false;
-  bool? officercheck=false;
+  bool? farmercheck = false;
+  bool? officercheck = false;
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-          child:Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Image(image: AssetImage("assets/Logo.png")),
+          ),
+          Expanded(
+              child: Form(
+                  child: Column(
             children: [
-              Center(
-                child:Image(image: AssetImage(
-                    "assets/Logo.png"
-                )
-                ),
-              ),
-              Expanded(
-                  child: Form(
-                      child: Column(
+              Container(
+                  margin:
+                      EdgeInsets.only(left: 50, right: 50, bottom: 60, top: 40),
+                  child: Column(
+                    children: [
+                      TxtInput(labelTxt: "Email"),
+                      TxtInput(labelTxt: "Password"),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Container(
-                              margin: EdgeInsets.only(left: 50,right: 50,bottom: 60,top: 40),
-                              child:Column(
+                            margin:
+                                EdgeInsets.only(left: 5, bottom: 10, top: 20),
+                            child: Text(
+                              "I am a,",
+                              style: TextStyle(
+                                  fontSize: 25, fontWeight: FontWeight.w700),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Row(
                                 children: [
-                                  TxtInput(labelTxt: "Email"),
-                                  TxtInput(labelTxt: "Password"),
-                                  Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-
-                                    children: [
-                                      Container(
-                                        margin: EdgeInsets.only(left: 5,bottom: 10,top: 20),
-                                        child: Text("I am a,",
-                                          style: TextStyle(
-                                              fontSize: 25,
-                                              fontWeight: FontWeight.w700
-                                          ),
-                                        ),
-                                      ),
-                                      Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                        children: [
-                                          Row(
-                                            children: [
-                                              Checkbox(
-                                                activeColor: Colors.green,
-                                                side: BorderSide(
-                                                    color: Colors.green,
-                                                    width: 3
-                                                ),
-                                                value: farmercheck,
-                                                onChanged: (Value){
-                                                  setState(() {
-                                                    officercheck=false;
-                                                    farmercheck=Value;
-                                                  });
-
-
-
-                                                },
-
-                                              ),
-                                              Text(
-                                                "Farmer",
-                                                style: TextStyle(
-                                                    fontSize: 20,
-                                                    fontWeight: FontWeight.w400
-                                                ),
-                                              )
-                                            ],
-                                          ),
-
-                                          Row(
-                                            children: [
-                                              Checkbox(
-                                                activeColor: Colors.green,
-                                                side: BorderSide(
-                                                    color: Colors.green,
-                                                    width: 3
-                                                ),
-                                                value:officercheck,
-                                                onChanged: (vaue){
-                                                  setState(() {
-                                                    farmercheck=false;
-                                                    officercheck=vaue;
-                                                  }
-                                                  );
-
-                                                },
-
-                                              ),
-                                              Text(
-                                                "Officer",
-                                                style: TextStyle(
-                                                    fontSize: 20,
-                                                    fontWeight: FontWeight.w400
-                                                ),
-                                              )
-                                            ],
-                                          )
-
-                                        ],
-                                      ),
-                                      CustomBtn(click: (){
-
-                                      },
-                                        txt: "Log In",
-                                        color: Colors.green,
-                                      ),
-
-                                      CustomBtn(click: (){
-                                        Navigator.pushNamed(context, '/signUp');
-                                      },
-                                        txt: "Go to Sign Up",
-                                        color: Colors.teal,
-                                      ),
-
-
-                                    ],
+                                  Checkbox(
+                                    activeColor: Colors.green,
+                                    side: BorderSide(
+                                        color: Colors.green, width: 3),
+                                    value: farmercheck,
+                                    onChanged: (Value) {
+                                      setState(() {
+                                        officercheck = false;
+                                        farmercheck = Value;
+                                      });
+                                    },
                                   ),
-
+                                  Text(
+                                    "Farmer",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w400),
+                                  )
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Checkbox(
+                                    activeColor: Colors.green,
+                                    side: BorderSide(
+                                        color: Colors.green, width: 3),
+                                    value: officercheck,
+                                    onChanged: (vaue) {
+                                      setState(() {
+                                        farmercheck = false;
+                                        officercheck = vaue;
+                                      });
+                                    },
+                                  ),
+                                  Text(
+                                    "Officer",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w400),
+                                  )
                                 ],
                               )
+                            ],
+                          ),
+                          CustomBtn(
+                            click: () {},
+                            txt: "Log In",
+                            color: Colors.green,
+                          ),
+                          CustomBtn(
+                            click: () {
+                              Navigator.pushNamed(context, '/signUp');
+                            },
+                            txt: "Go to Sign Up",
+                            color: Colors.teal,
                           ),
                         ],
-                      )
-                  )
-              )
+                      ),
+                    ],
+                  )),
             ],
-          )),
+          )))
+        ],
+      )),
     );
   }
 }
-
-
-
-
-
-
